bd224310f72dda330bf5228410f8c8ccfd97825b
Kazuaki Nishizawa <kazuaki_nishizawa@hotmail.com>
2012-08-24 22:01:51 +0900

    example06 time for sum loop

---
+++ example06_time_sum/example06_time_sum.js
@@ -0,0 +1,14 @@
+﻿// time for sum loop
+
+var max = 100000000;
+
+console.time(max + '要素');
+
+var sum = 0;
+
+for (var i = 0; i < max; i++) {
+  sum += i;
+}
+console.log('sum=' + sum);
+
+console.timeEnd(max + '要素');
