3a7071af4f5309128f061e063a4017ce30a3c793
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2012-08-29 14:12:37 +0900

    example11 http client for Facebook Graph API

---
+++ example11_http_client_facebook_graph_api/example11_http_client_facebook_com_kazuaki_nishizawa.js
@@ -0,0 +1,47 @@
+// http_client.js
+
+var options = {
+  host: 'graph.facebook.com',
+  port: 80,
+  path: '/kazuaki.nishizawa',
+};
+
+var http = require('http');
+var util = require('util');
+
+console.log('OPTIONS:');
+console.log(util.inspect(options, showHidden=false, depth=null, color=true));
+console.log();
+
+http.get(options, function(resp) {
+  console.log('STATUS: ' + resp.statusCode);
+  console.log();
+
+  //console.log('HEADERS: ' + JSON.stringify(resp.headers));
+  console.log('HEADERS: ');
+  console.log(util.inspect(resp.headers, showHidden=false, depth=null, color=true));
+  console.log();
+
+  resp.setEncoding('utf8');
+
+  resp.on('data', function (chunk) {
+    console.log('BODY: data chunk legth='+chunk.length);
+    console.log(chunk);
+
+    // ‘Sdata‚ª1‰ñ‚ÅŽæ“¾‚Å‚«‚Ä‚¢‚é‘O’ñ
+    var graph = eval('('+chunk+')');
+    console.log();
+    console.log('GRAPH:');
+    console.log(util.inspect(graph, showHidden=false, depth=null, color=true));
+    console.log();
+
+    console.log('JSON:');
+    console.log(JSON.stringify(graph));
+    console.log();
+
+  });
+
+  resp.on('end', function () {
+    console.log('END:');
+  });
+});
