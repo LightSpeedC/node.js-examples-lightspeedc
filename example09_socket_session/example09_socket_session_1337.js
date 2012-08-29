82217f3cc73555dec117ed78d6cfc0b35fd3aef6
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2012-08-29 13:22:53 +0900

    example09 socket server with session

--- example09_socket_session/example09_socket_session_1337.js
+++ example09_socket_session/example09_socket_session_1337.js
@@ -25,18 +25,17 @@ var counter = 0;
 
 var server = net.createServer(function (socket) {
   ++counter;
-  socket.date1 = new Date();
-  socket.count1 = counter;
-  console.log(socket.count1+' '+DateString(socket.date1));
-  socket.write(socket.count1+' '+DateString(socket.date1)+'\r\n');
-  socket.on('data', function(chunk) {
-    console.log(socket.count1+' '+chunk.toString()+' '+DateString(socket.date1));
-    socket.write(socket.count1+' '+chunk.toString()+' '+DateString(socket.date1)+'\r\n');
-  });
   socket.write('Echo server\r\n');
+  socket.date1 = DateString(new Date());
+  socket.count1 = counter;
+  console.log(socket.date1+' '+socket.count1+' New Session');
+  socket.write(socket.date1+' '+socket.count1+' New Session\r\n');
+  socket.on('data', function(chunk) {
+    console.log(socket.date1+' '+socket.count1+' data:'+chunk.toString());
+    socket.write(socket.date1+' '+socket.count1+' data:'+chunk.toString()+'\r\n');
+  });
   //socket.pipe(socket);
 });
 
 server.listen(1337, '127.0.0.1');
 console.log('telnet 127.0.0.1 1337');
-
